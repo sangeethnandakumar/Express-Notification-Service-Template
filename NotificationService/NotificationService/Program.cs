74fb0e3d0e6518ef08b7c57714aa21d600234997
Sangeeth Nandakumar <sangeethnandakumar@gmail.com>
2020-10-10 22:46:25 +0530

    Update Program.cs

--- NotificationService/NotificationService/Program.cs
+++ NotificationService/NotificationService/Program.cs
@@ -33,6 +33,7 @@ namespace NotificationService
                     });
 
                     services.AddHostedService<Worker>();
-                });
+                })
+                .UseWindowsService();;
     }
 }
